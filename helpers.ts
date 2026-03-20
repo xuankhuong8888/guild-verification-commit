770a3b6b371d1001fc084b6ae560fb115fda41af
xuankhuong8888 <xuankhuong8888@icloud.com>
2026-03-20 09:48:37 +0700

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-20T02:48:08.540Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-20T02:48:36.299Z
+Commit: Patch API endpoint error handling
