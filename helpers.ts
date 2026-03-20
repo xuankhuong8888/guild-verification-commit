7a9d281750d472698b5d06284b74fc8809432f10
xuankhuong8888 <xuankhuong8888@icloud.com>
2026-03-20 09:48:57 +0700

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-20T02:48:54.357Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-20T02:48:55.903Z
+Commit: Fix CSS styling bug on mobile devices
